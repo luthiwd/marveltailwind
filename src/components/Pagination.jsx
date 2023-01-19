acb2323818f2a05440a938cf41e7fe0b9b8aedb9
Luthiwd <annattar@gmail.com>
2023-01-19 21:54:27 +0100

    Advanced in pagination tailwind, Characters details solved error and new view

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -12,11 +12,11 @@ function Pagination({setOffset,offset,currentPage, setCurrentPage}) {
   }
   
   return (
-    <div>{
-      !offset === 0 ? <button onClick={handlePrevClick}>Previous</button> : <></>}
-      <button onClick={handlePrevClick} disabled={offset === 0} hidden={offset === 0}>Previous</button>
-      <span>Page {currentPage}</span>
-      <button onClick={handleNextClick}>Next</button>
+    <div className='grid-cols-3 place-content-center justify-center'>{
+      !offset === 0 ? <button onClick={handlePrevClick}>Previous</button> : <div className="bg-gradient-to-r from-slate-300 to-black rounded-2xl px-2 mx-2 text-white w-40"></div>}
+      <button className="bg-gradient-to-r from-slate-300 to-black rounded-2xl px-2 mx-2 text-white w-40" onClick={handlePrevClick} disabled={offset === 0} hidden={offset === 0}>Previous</button>
+      <span className='bg-gradient-to-r from-slate-300 to-slate-600 rounded-2xl px-2 mx-2 w-40'>Page {currentPage}</span>
+      <button className='bg-gradient-to-l from-slate-300 to-black rounded-2xl px-2 mx-2 text-white w-40' onClick={handleNextClick}>Next</button>
     </div>
   )
 }
--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -1,46 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { charDetailsService } from '../services/characters.services'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { charDetailsService } from "../services/characters.services";
 
 function CharacterDetailsPage() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [charDetails, setCharDetails] = useState([]);
+  const [fetching, isFetching] = useState(true);
 
-  const navigate = useNavigate ()
-  const { id } = useParams()
-  const [charDetails, setCharDetails] = useState("")
-  const [fetching, isFetching] = useState(false)
-  
   useEffect(() => {
-    getChar()
-  },[])
+    getChar();
+  }, []);
 
   const getChar = async () => {
     try {
-      const infoChar = await charDetailsService(id)
-      setCharDetails(infoChar.data.data.results)
-      console.log("detalles", infoChar.data.data.results)
-      isFetching(true)
+      const detailsChar = await charDetailsService(id);
+      setCharDetails(detailsChar.data.data.results);
+      console.log(detailsChar.data.data.results);
+      isFetching(false);
     } catch (error) {
-      navigate('/error')
+      navigate("/error");
     }
+  };
+
+  if (fetching) {
+    return <div>Loading...</div>;
   }
 
-  if (!fetching) {
-    return (
-      <div>
-        Loading...
-      </div>
-    );
-  }
-  
   return (
-    <div className='place-content-center sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 h-50 font-mono pt-20'>
-      <div className='grid place-content-center'>
-        {/* <img className='sm:w-48 lg:w-72 rounded-2xl' src={`${charDetails.thumbnail.path}.${charDetails.thumbnail.extension}`} alt={charDetails.name}/> */}
-          {charDetails.name}
-      </div>
-  
+    <div className="flex place-content-center justify-center">
+      {charDetails.map((eachChar) => {
+        return (
+          <div key={eachChar.id} className="flex h-50 font-mono pt-20">
+            <div className="place-content-center w-1/2 mx-2">
+              <img
+                className="rounded-2xl"
+                src={`${eachChar.thumbnail.path}.${eachChar.thumbnail.extension}`}
+                alt={eachChar.name}
+              />
+              {eachChar.name}
+            </div>
+            <div className="w-1/2 mx-2">
+              {eachChar.description}
+              <div className="m-2">
+              <h1 className="m-2">Comics</h1>
+                {eachChar.comics.items.map((eachComic) =>{
+                  return (
+                    <>
+                      {eachComic.name}
+                    </>
+                  )
+                })}
+              </div>
+            </div>.
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default CharacterDetailsPage
+export default CharacterDetailsPage;
